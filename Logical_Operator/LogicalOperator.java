061b1270cde3d2a151b516391df1da44756dc2d2
chaurasiyaharish <chaurasiyah348@gmail.com>
2024-10-18 19:19:08 +0530

    logical operator

---
+++ Logical_Operator/LogicalOperator.java
@@ -0,0 +1,13 @@
+package Logical_Operator;
+public class LogicalOperator {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a=10;
+        b=20;
+        c=30;
+        d=40;
+        boolean r = a<b && c<d;
+        System.out.println("Result : "+r);
+    }
+    
+}
---
+++ Logical_Operator/OrOperator.java
@@ -0,0 +1,19 @@
+
+
+package Logical_Operator;
+
+/**
+ * OrOperator
+ */
+public class OrOperator {
+
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a=10;
+        b=20;
+        c=30;
+        d=40;
+        boolean r = a<b || c>d;
+        System.out.println("Result : "+r);
+    }
+}
